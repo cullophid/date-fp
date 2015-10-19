130603a308c284c285ad6b65b06486e637f35ade
Andreas Møller <andreas.moller@gmail.com>
2015-10-19 19:36:53 +0100

    switched from browserify to webpack
    Modules built with browserify does not work properly with.... browserify...
    see: https://github.com/cullophid/date-fp/issues/8
    
    added .eslintrc
    I am a big fan of eslint.
    This is a base config file for eslint, more or less set up based on my personal preferences.
    As more people ptich in their ideas pad opinions, we will evolve the file.
    I have not added code linting as a build step
    as I prefer to just use it in my editor while developing.
    This is again somthing that there should be a general concensus about.

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+/*eslint-disable*/
+module.exports = {
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      { test: /\.js|.jsx$/, exclude: /node_modules/, loader: "babel-loader"}
+    ]
+  },
+  output: {
+     libraryTarget: "umd",
+     library: "D"
+   },
+};
