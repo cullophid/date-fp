5e24067e0e7552f512ae70cc7d5acefc8eb21b14
stefano <svozza@gmail.com>
2015-10-24 21:13:19 +0100

    fix build

--- src/isLeapYear.js
+++ src/isLeapYear.js
@@ -3,6 +3,6 @@ import curry from 'lodash.curry';
 import isValid from './isValid';
 
 export default curry((date) => {
-    return isValid(date) ? new Date(date.getFullYear(), 1, 29).getMonth() === 1 :
+    return isValid(date) ? new Date(date.getFullYear() + '-02-29').getMonth() === 1 :
         new Error('Invalid date object provided.');
 });
