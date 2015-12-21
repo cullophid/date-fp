97a44bf079ca663d891206d62b3523d1d7ba6227
Andreas Møller <andreasmoller@Andreass-MacBook.local>
2015-12-21 12:54:12 +0000

    updated isValid to use new function checkNaN
    isNaN does type coercision and is there for error prone
    
    Number.isNaN() is es6 and is not translated by babel

--- src/isValid.js
+++ src/isValid.js
@@ -1,7 +1,12 @@
+/* eslint no-self-compare: 0*/
 import curry from 'lodash.curry'
+
+const checkNaN = (n) => {
+  return isNaN(n) && n !== n
+}
 
 export default curry((date) => {
   if (Object.prototype.toString.call(date) !== "[object Date]") return false
 
-  return !isNaN(date.getTime())
+  return !checkNaN(date.getTime())
 })
