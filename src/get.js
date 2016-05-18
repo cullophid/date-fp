64f35867a08fd49074edeb3a94b33e63d8820c1c
Andreas Møller <andreas.moller@gmail.com>
2016-05-18 23:42:17 +0100

    added get week, and updated format to include weeknumbers

--- src/get.js
+++ src/get.js
@@ -1,10 +1,29 @@
 import curry from 'lodash.curry'
+
+import add from './add'
+import {DATE_UNITS} from './helpers/constants'
+
+
+const getWeek = _date => {
+  const getWeekDay = d => d.getDay() ? d.getDay() : 7
+  const date = new Date(_date)
+  // 4th of jan is always week 1. This is because week numbers are rediculous and make no logical sense
+  const firstWeek = new Date(date.getFullYear(), 0, 4)
+  // set the weekday to sunday
+  date.setDate(date.getDate() + (7 - getWeekDay(date)))
+  firstWeek.setDate(firstWeek.getDate() + (7 - getWeekDay(firstWeek)))
+  console.log('dates', date, firstWeek)
+  // return the diff in weeks. add 1 because we are starting at week 1
+  return  1 + Math.round((date.getTime() - firstWeek.getTime()) / DATE_UNITS.weeks)
+}
+
 const getters = {
   'milliseconds': date => date.getMilliseconds(),
   'seconds': date => date.getSeconds(),
   'minutes': date => date.getMinutes(),
   'hours': date => date.getHours(),
   'date': date => date.getDate(),
+  'week': getWeek,
   'month': date => date.getMonth() + 1,
   'year': date => date.getFullYear()
 }
